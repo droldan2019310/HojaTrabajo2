24ab83a8f8983f0c0ee3498969b8978c7ff151e6
droldan-2019310 <droldan-2019310@kinal.edu.gt>
2023-02-01 22:26:50 -0600

    add stack clas

--- src/models/Stack.java
+++ src/models/Stack.java
@@ -5,23 +5,28 @@ import java.util.ArrayList;
 import interfaces.IStack;
 
 public class Stack<T> implements IStack<T> {
-
+    private ArrayList<T> numbers = new ArrayList<T>();
+    int position=-1;
     @Override
     public void push(T item) {
         // TODO Auto-generated method stub
-        
+        numbers.add(item);
+        position++;
     }
 
     @Override
     public T pop() {
         // TODO Auto-generated method stub
-        return null;
+        T a = numbers.get(position);
+        numbers.remove(position);
+        position--;
+        return a;
     }
 
     @Override
     public T peek() {
         // TODO Auto-generated method stub
-        return null;
+        return numbers.get(position);
     }
 
     @Override
@@ -29,6 +34,7 @@ public class Stack<T> implements IStack<T> {
         // TODO Auto-generated method stub
         return 0;
     }
+    
     
 
 }
